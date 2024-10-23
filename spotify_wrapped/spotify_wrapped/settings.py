20e8b38b3539c84d3d2e1773f58fc030ca100ff0
Mokshith <mokshithm15@gmail.com>
2024-10-23 13:45:48 -0400

    finished entire homepage

--- spotify_wrapped/spotify_wrapped/settings.py
+++ spotify_wrapped/spotify_wrapped/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "core"
 ]
 
 MIDDLEWARE = [
@@ -53,15 +54,15 @@ ROOT_URLCONF = "spotify_wrapped.urls"
 
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],  # This should be empty if you're using app-based templates
+        'APP_DIRS': True,  # This must be True to tell Django to look in app's 'templates' folder
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
