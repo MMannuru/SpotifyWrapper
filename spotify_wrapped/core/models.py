04f5768726be3754200f0c35b16a62b6b8401d10
karenlu0 <karenlu75@yahoo.com>
2024-10-30 06:51:36 -0400

    added my branch changes

--- spotify_wrapped/core/models.py
+++ spotify_wrapped/core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=255)
+
+class Song(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    duration = models.IntegerField()  # Duration in seconds
+
+class UserListeningData(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    play_count = models.IntegerField(default=0)
