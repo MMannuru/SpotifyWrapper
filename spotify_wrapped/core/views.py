e899fd135de14d817e26e7649836add26bd201b9
chilukam04 <chilukam04@gmail.com>
2024-11-30 02:04:35 -0500

    took out some of the hosted login

--- spotify_wrapped/core/views.py
+++ spotify_wrapped/core/views.py
@@ -222,8 +222,8 @@ def spotify_auth_url(request):
     params = {
         "client_id": CLIENT_ID,
         "response_type": "code",
-        #"redirect_uri": redirect_uri,
-        "redirect_uri": 'https://spotifywrapper.onrender.com/callback/',
+        "redirect_uri": redirect_uri,
+        #"redirect_uri": 'https://spotifywrapper.onrender.com/callback/',
         "scope": SCOPE,
         #"show_dialog": "true",  # Force Spotify to show the login dialog
     }
@@ -274,9 +274,9 @@ def get_redirect_uri(request):
     Returns:
         str: The dynamically generated redirect URI.
     """
-    #host = request.get_host()  # Get the host, e.g., '128.61.9.117:8000'
-    #return f"http://{host}/callback/"
-    return f"https://spotifywrapper.onrender.com/callback/"
+    host = request.get_host()  # Get the host, e.g., '128.61.9.117:8000'
+    return f"http://{host}/callback/"
+    #return f"https://spotifywrapper.onrender.com/callback/"
 
 
 
